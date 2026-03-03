e9a0c887d1f4ac22fdc711d97edc85f787a0dd0c
Hùng <suga4026@gmail.com>
2026-03-03 20:40:08 +0700

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -51,7 +51,7 @@ const Footer: React.FC = () => {
               <li>
                 <a 
                 href="/#home" 
-                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400"
+                  className="text-gray-800 transition-colors duration-300 dark:text-white hover:text-blue-400"
                 >
                   {t('Home')}
                 </a>
@@ -59,7 +59,7 @@ const Footer: React.FC = () => {
               <li>
                 <a 
                 href="/#about" 
-                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400"
+                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400 dark:text-white"
                 >
                   {t('About')}
                 </a>
@@ -67,7 +67,7 @@ const Footer: React.FC = () => {
               <li>
                 <a 
                 href="/#skills" 
-                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400"
+                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400 dark:text-white"
                 >
                   {t('Skills')}
                 </a>
@@ -75,7 +75,7 @@ const Footer: React.FC = () => {
               <li>
                 <a 
                 href="/#projects" 
-                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400"
+                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400 dark:text-white"
                 >
                   {t('Projects')}
                 </a>
@@ -83,7 +83,7 @@ const Footer: React.FC = () => {
               <li>
                 <a 
                 href="/#experience" 
-                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400"
+                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400 dark:text-white"
                 >
                   {t('Experience')}
                 </a>
@@ -91,7 +91,7 @@ const Footer: React.FC = () => {
               <li>
                 <a 
                 href="/#contact" 
-                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400"
+                  className="text-gray-800 transition-colors duration-300 hover:text-blue-400 dark:text-white"
                 >
                   {t('Contact')}
                 </a>
@@ -104,15 +104,15 @@ const Footer: React.FC = () => {
             <ul className="space-y-3">
               <li className="flex items-start">
                 <svg className="w-5 h-5 text-gray-800 mr-2 mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
-                <span className="text-gray-800">nshung0105@gmail.com</span>
+                <span className="text-gray-800 dark:text-white">nshung0105@gmail.com</span>
               </li>
               <li className="flex items-start">
                 <svg className="w-5 h-5 text-gray-800 mr-2 mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
-                <span className="text-gray-800">+84-857000163</span>
+                <span className="text-gray-800 dark:text-white">+84-857000163</span>
               </li>
               <li className="flex items-start">
                 <svg className="w-5 h-5 text-gray-800 mr-2 mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                <span className="text-gray-800">Tp Đà Nẵng, Việt Nam</span>
+                <span className="text-gray-800 dark:text-white">Tp Đà Nẵng, Việt Nam</span>
               </li>
             </ul>
           </div>
